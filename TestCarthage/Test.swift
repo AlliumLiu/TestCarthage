e8baa3a9a6e7a09d71d56a3341b53a556d52b372
cong.liu01 <cong.liu01@liulishuo.com>
2019-09-24 15:31:23 +0800

    first commit

---
+++ TestCarthage/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  TestCarthage
+//
+//  Created by cong liu on 2019/9/24.
+//  Copyright © 2019 cong liu. All rights reserved.
+//
+
+import Foundation
+
+public class Test {
+  public func printTest() {
+    print("test test ....")
+  }
+}
