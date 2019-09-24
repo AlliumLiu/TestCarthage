beaf59f1c34773cd4a755af2d1e4141f1b050c94
cong.liu01 <cong.liu01@liulishuo.com>
2019-09-24 17:00:55 +0800

    view

--- TestCarthage/Test.swift
+++ TestCarthage/Test.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 import Hydra
 
 public class Test: NSObject {
@@ -17,4 +18,25 @@ public class Test: NSObject {
   public static func printHydra() {
     print(HydraVersionNumber)
   }
+}
+
+
+public class TestView: UIView {
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.backgroundColor = .red
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  public func printFrame() {
+    print(frame)
+  }
+  
+  public func printHydra() {
+    print(HydraVersionNumber)
+  }
+
 }
